88db59a89a7fd6bb754dae86783630127910ed0c
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2020-06-12 19:30:38 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,61 +1,127 @@
 ///////////////////////////////////////////////////////////////////////
 /// Config
+// Импортируем модуль для работы с tcp server
 const net = require("net");
+// Импортируем модуль для работы с консолью
 const { exec } = require("child_process");
-const server = net.createServer();
-const port = process.env.PORT || 2222;
-const msgLog = msg => console.log(`Client says: ${msg}`);
+// Создаем ТСР сервер
+const tcpServer = net.createServer();
+// Указываем порт для него
+const port = 2222;
+// Сокращение для console.log
 const log = console.log;
+// Последний текущий путь, заданный пользователем
+let currentPath;
 ///////////////////////////////////////////////////////////////////////
 /// Event handlers
-server.on("close", () => {
-  log("Server closed!");
+// Когда сервер отключается, пишем об этом
+tcpServer.on("close", () => {
+  log("Сервер отключился");
 });
 
-server.on("connection", function (socket) {
-  socket.setEncoding('utf8'); 
-  socket.write("Plase execute commans on server.");
-  socket.on("data", data => {
-    msgLog(data);
-    log(`Executing command "${data}"...`)
-    exec(data.toString(), (err, stdout, stderr) => {
-      if (err || stderr.length > 0) {
-        console.error(err)
-        socket.write(stderr || err || "Error happened!");
-      } else {
-        log(stdout);
-        socket.write(stdout || "Command executed");
+//Когда к серверу подконнектился клиент, делаем что-то...
+tcpServer.on("connection", function (client) {
+  // Русские символы
+  client.setEncoding("utf8");
+  // Пишем в консоль
+  log("TCP Клиент подключился к данному серверу");
+  // Пишем на клиент
+  client.write("Клиент начинает исполнять команды на данном сервере...");
+  // Когда от клиента пришла какая-то команда
+  client.on("data", function (data) {
+    // Обрабатываем её
+    data = prepareDataForExecution(data);
+    // Если в отправленной пользователем команде содержится CD
+    if (data.includes("cd ")) {
+      // То мы сохраняем новый путь, и при открытии консоли сначала переходим по нужному пути, а потом уже исполняем саму команду
+      if (currentPath) {
+        data = insertCurrentPathToData(currentPath, data);
       }
-    });
+      else {
+        data = `${data} && cd`;
+      }
+      log(`Клиент исполнил команду ${data}`);
+      // Исполнить полученную команду
+      exec(data.toString(), function(err, stdout, stderr) {
+        //Если возникла ошибка, написать об этом клиенту
+        if (err || stderr.length > 0) {
+          log("Команду выполнить невозможно! Ожидание следующей команды");
+          client.write("Команду выполнить невозможно! Ожидание следующей команды");
+        } 
+        // Если всё хорошо
+        else {
+          // Пишем что всё хорошо
+          log(stdout);
+          // Пишем текущий путь
+          currentPath = stdout.replace(/\r?\n|\r/g, "").trim();
+          log(`Текущая директория: ${currentPath}`);
+          client.write(stdout || "Команда выполнена.");
+        }
+      });
+    } 
+    else {
+      if (currentPath) {
+        // Вставляем перед ней текущий путь
+        data = `cd "${currentPath}" && ${data.toString()}`;
+      }
+      log(`Клиент исполнил команду ${data}`);
+      // Исполняем её
+      exec(data, (err, stdout, stderr) => {
+        if (err || stderr.length > 0) {
+          log("Команду выполнить невозможно! Ожидание следующей команды");
+          client.write("Команду выполнить невозможно! Ожидание следующей команды");
+        } 
+        else {
+          log(stdout);
+          client.write(stdout || "Команда выполнена");
+        }
+      });
+    }
   });
 
-  socket.on("error", function (error) {
-    log("Error: " + error);
+  // Подготавливаем команду с клиента для выполнения
+  function prepareDataForExecution(data) {
+    // Удаляем все пробелы
+    data = data.trim();
+    // Удаляем лишние символы
+    data = data.replace(/\r?\n|\r/g, "");
+    return data;
+  }
+
+  function insertCurrentPathToData(currentPath, data) {
+    data = `cd "${currentPath}" && ${data} && cd`;
+    return data;
+  }
+ 
+  ///////////////////////////////////////////////////////////////////////
+  client.on("error", function (error) {
+    log("Ошибка: " + error);
   });
 
-  socket.on("timeout", function () {
-    log("Socket timed out!");
-    socket.end("Timed out!");
+  client.on("timeout", function () {
+    log("Нет ответа от клиентп");
+    client.end("Нет ответа");
   });
 
-  socket.on("end", function (data) {
-    log("Socket ended from other end!");
-    log("End data : " + data);
+  client.on("end", function (data) {
+    log("Клиент прервал сеанс");
+    log("Данные: " + data);
   });
 
-  socket.on("close", function (error) {
-    log("Socket closed!");
+  client.on("close", function (error) {
+    log("Соединение с клиентом закрылось");
   });
+  ///////////////////////////////////////////////////////////////////////
+});
+/////////////////////////////////////////////////////////////////////////
+tcpServer.on("error", (error) => {
+  log(`Возникла ошибка: ${error}`);
 });
 
-server.on("error", error => {
-  log(`Error: ${error}`);
-});
-
-server.on("listening", () => {
-  log(`Server is listening on port ${port}`);
+tcpServer.on("listening", () => {
+  log(`TCP Сервер запущен на порту ${port}`);
 });
 ///////////////////////////////////////////////////////////////////////
-/// Starting  
-server.listen(port);
+/// Starting
+tcpServer.listen(port);
 ///////////////////////////////////////////////////////////////////////
